baf8e6857ecf08fe26ecb244179bd75e682e17a1
imachol <ido.machol@bcm.edu>
2014-12-09 13:59:57 -0600

    Add copyright information and unified header comment.

--- src/juicebox/encode/EncodeFileBrowser.java
+++ src/juicebox/encode/EncodeFileBrowser.java
@@ -1,5 +1,12 @@
 /*
- * Created by JFormDesigner on Thu Oct 31 22:31:02 EDT 2013
+ * Copyright (C) 2011-2014 Aiden Lab - All Rights Reserved
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Aiden Lab All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. Aiden Lab is not responsible for its use, misuse, or functionality.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
  */
 
 package juicebox.encode;
--- src/juicebox/matrix/SparseVector.java
+++ src/juicebox/matrix/SparseVector.java
@@ -1,22 +1,15 @@
-package juicebox.matrix;
 /*
- * Copyright (c) 2007-2014 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ * Copyright (C) 2011-2014 Aiden Lab - All Rights Reserved
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Aiden Lab All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. Aiden Lab is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 
+package juicebox.matrix;
 
 import java.util.Collection;
 import java.util.HashMap;
