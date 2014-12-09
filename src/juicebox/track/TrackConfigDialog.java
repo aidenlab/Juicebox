fd1bb5818a6f491e43accf67b437b205e774ab38
imachol <ido.machol@bcm.edu>
2014-12-09 13:54:12 -0600

    Add copyright information and unified header comment.

--- src/juicebox/track/TrackConfigDialog.java
+++ src/juicebox/track/TrackConfigDialog.java
@@ -1,5 +1,12 @@
 /*
- * Created by JFormDesigner on Thu Aug 08 17:01:58 EDT 2013
+ * Copyright (C) 2011-2014 Aiden Lab - All Rights Reserved
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Aiden Lab All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. Aiden Lab is not responsible for its use, misuse, or functionality.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
  */
 
 package juicebox.track;
--- src/juicebox/windowui/LoadDialog.java
+++ src/juicebox/windowui/LoadDialog.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2011-2014 Aiden Lab - All Rights Reserved
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Aiden Lab All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. Aiden Lab is not responsible for its use, misuse, or functionality.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ */
+
 package juicebox.windowui;
 
 import juicebox.HiCGlobals;
