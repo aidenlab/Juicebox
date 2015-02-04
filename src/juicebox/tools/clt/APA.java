4922d92f3c33437fb51b93eac1b42121c3a6fccb
Muhammad S Shamim <sa501428@gmail.com>
2015-02-04 16:40:35 -0600

    APA fixes

--- src/juicebox/tools/clt/APA.java
+++ src/juicebox/tools/clt/APA.java
@@ -28,7 +28,6 @@ import juicebox.data.*;
 import juicebox.tools.HiCTools;
 import juicebox.track.Feature2D;
 import juicebox.windowui.HiCZoom;
-import juicebox.windowui.NormalizationType;
 import org.apache.commons.math.linear.Array2DRowRealMatrix;
 import org.broad.igv.Globals;
 import org.broad.igv.feature.Chromosome;
@@ -36,11 +35,12 @@ import org.broad.igv.feature.Chromosome;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+/**
+ *
+ * TODO - once fully debugged, change notation convention from underscore to camelcase (match the rest of the files)
+ */
 public class APA extends JuiceboxCLT {
 
     private Array2DRowRealMatrix xMatrix;
@@ -60,7 +60,7 @@ public class APA extends JuiceboxCLT {
     int width=6; //size of boxes
     int peakwidth = 2; //for enrichment calculation of crosshair norm
     private int resolution = 10000;
-    boolean save_all = false;
+    boolean saveAllData = true;
 
     private final String workingdirectory = System.getProperty("user.dir");
     private String dataFolder = workingdirectory +"/Data";
@@ -68,12 +68,12 @@ public class APA extends JuiceboxCLT {
     @Override
     public void readArguments(String[] args, HiCTools.CommandLineParser parser) throws IOException {
 
-        if (!(args.length > 2 && args.length < 6)) {
+        if (!(args.length > 3 && args.length < 7)) {
             throw new IOException("1");
         }
 
-        files = new String[args.length];
-        System.arraycopy(args, 0, files, 0, files.length);
+        files = new String[args.length-1];
+        System.arraycopy(args, 1, files, 0, files.length-1);
 
         if (files.length > 4)
             restrictionSiteFilename = files[4];
@@ -104,17 +104,18 @@ public class APA extends JuiceboxCLT {
         int mdpt = shift.length/2;
 
         //define gw data structures
-        int GW_npeaks = 0;
-        int GW_npeaks_used = 0;
-        int GW_npeaks_used_nonunique = 0;
-        Array2DRowRealMatrix GW_psea = cleanArray2DMatrix(L,L);
-        Array2DRowRealMatrix GW_normed_psea = cleanArray2DMatrix(L,L);
-        Array2DRowRealMatrix GW_center_normed_psea = cleanArray2DMatrix(L,L);
-        Array2DRowRealMatrix GW_rank_psea = cleanArray2DMatrix(L,L);
-        Array2DRowRealMatrix GW_coverage = cleanArray2DMatrix(L,L);
-        double[] GW_enhancement = new double [0];
+        int gw_npeaks = 0;
+        int gw_npeaks_used = 0;
+        int gw_npeaks_used_nonunique = 0;
+        Array2DRowRealMatrix gw_psea = APAUtils.cleanArray2DMatrix(L, L);
+        Array2DRowRealMatrix gw_normed_psea = APAUtils.cleanArray2DMatrix(L, L);
+        Array2DRowRealMatrix gw_center_normed_psea = APAUtils.cleanArray2DMatrix(L, L);
+        Array2DRowRealMatrix gw_rank_psea = APAUtils.cleanArray2DMatrix(L, L);
+        Array2DRowRealMatrix gw_coverage = APAUtils.cleanArray2DMatrix(L, L);
+        List<Double> gw_enhancement = new ArrayList<Double>();
 
         try {
+            System.out.println("Accessing "+files[0]);
             DatasetReaderV2 reader = new DatasetReaderV2(files[0]);
             Dataset ds = reader.read();
 
@@ -127,83 +128,94 @@ public class APA extends JuiceboxCLT {
             Map<Chromosome,ArrayList<Feature2D>> chrToLoops =
                     APAUtils.loadLoopList(files[1], new ArrayList<Chromosome>(chromosomes), min_peak_dist, max_peak_dist);
 
+            Set<Chromosome> commonChromosomes = getSetIntersection(chrToLoops.keySet(),
+                    new HashSet<Chromosome>(chromosomes));
+
             // Loop through chromosomes
-            for (Chromosome chr : chromosomes) {
+            for (Chromosome chr : commonChromosomes) {
 
-                Array2DRowRealMatrix psea = cleanArray2DMatrix(L,L);
-                Array2DRowRealMatrix normed_psea = cleanArray2DMatrix(L,L);
-                Array2DRowRealMatrix center_normed_psea = cleanArray2DMatrix(L,L);
-                Array2DRowRealMatrix rank_psea = cleanArray2DMatrix(L,L);
-                Array2DRowRealMatrix coverage = cleanArray2DMatrix(L,L);
-                double[] enhancement = new double [0];
-                int npeaks_used = 0, npeaks_used_nonunique;
 
+
+                Array2DRowRealMatrix psea = APAUtils.cleanArray2DMatrix(L, L);
+                Array2DRowRealMatrix normed_psea = APAUtils.cleanArray2DMatrix(L, L);
+                Array2DRowRealMatrix center_normed_psea = APAUtils.cleanArray2DMatrix(L, L);
+                Array2DRowRealMatrix rank_psea = APAUtils.cleanArray2DMatrix(L, L);
+                Array2DRowRealMatrix coverage = APAUtils.cleanArray2DMatrix(L, L);
+                List<Double> enhancement = new ArrayList<Double>();
 
                 if (chr.getName().equals(Globals.CHR_ALL)) continue;
 
-                ArrayList<Feature2D> loops = chrToLoops.get(chr);
+                // get all loops
+                if(chrToLoops.containsKey(chr)) {
+                    ArrayList<Feature2D> loops = chrToLoops.get(chr);
+                    int npeaks_used_nonunique = loops.size();
+                    gw_npeaks_used_nonunique += npeaks_used_nonunique;
 
+                    // remove repeats
+                    Set<Feature2D> uniqueLoops = new HashSet<Feature2D>(loops);
+                    int npeaks_used = loops.size();
+                    gw_npeaks_used += npeaks_used;
 
-                Matrix matrix = ds.getMatrix(chr, chr);
-                if (matrix == null) continue;
+                    Matrix matrix = ds.getMatrix(chr, chr);
+                    if (matrix == null) continue;
 
-                List<HiCZoom> resolutions = ds.getBpZooms();
-                HiCZoom zoom = resolutions.get(0);
-                int currentDistance = Math.abs(zoom.getBinSize() - resolution);
-                // Loop through resolutions
-                for (HiCZoom subZoom : resolutions) {
-                    int newDistance = Math.abs(subZoom.getBinSize() - resolution);
-                    if (newDistance < currentDistance) {
-                        currentDistance = newDistance;
-                        zoom=subZoom;
-                    }
-                }
-                MatrixZoomData zd = matrix.getZoomData(zoom);
-
-                // TODO loop num statistics
-
-                for (Feature2D loop : loops){
-
-                    int loopX = loop.getStart1(), loopY = loop.getStart2();
-
-                    int binXStart = loopX - resolution*(window+1);
-                    int binXEnd = loopX + resolution*(window+1);
-                    int binYStart = loopY - resolution*(window+1);
-                    int binYEnd = loopY + resolution*(window+1);
-
-                    List<Block> blocks = zd.getNormalizedBlocksOverlapping(binXStart, binYStart, binXEnd, binYEnd,
-                            NormalizationType.NONE);
-                    for (Block b : blocks) {
-                        for (ContactRecord rec : b.getContactRecords()) {
-                            //, rec.getBinY(), rec.getCounts()
-
-                            int relativeX = window + (rec.getBinX() - loopX)/resolution;
-                            int relativeY = window + (rec.getBinY() - loopY)/resolution;
-
-                            if(relativeX >= 0 && relativeX < L){
-                                if(relativeY >= 0 && relativeY < L){
-                                    psea.addToEntry(relativeX, relativeY, rec.getCounts());
-                                }
-                            }
-
+                    // select zoom level closest to the requested one
+                    List<HiCZoom> resolutions = ds.getBpZooms();
+                    HiCZoom zoom = resolutions.get(0);
+                    int currentDistance = Math.abs(zoom.getBinSize() - resolution);
+                    // Loop through resolutions
+                    for (HiCZoom subZoom : resolutions) {
+                        int newDistance = Math.abs(subZoom.getBinSize() - resolution);
+                        if (newDistance < currentDistance) {
+                            currentDistance = newDistance;
+                            zoom = subZoom;
                         }
                     }
+                    System.out.println("Adjusting zoom to " + zoom.getBinSize());
+                    MatrixZoomData zd = matrix.getZoomData(zoom);
+
+                    for (Feature2D loop : uniqueLoops) {
+                        //long time = System.nanoTime();
+                        System.out.println("Processing loop");
+
+                        //System.out.println((System.nanoTime()-time)/1000000);
+
+                        Array2DRowRealMatrix newData = APAUtils.extractLocalizedData(zd, loop, L, resolution, window);
+
+                        //System.out.println((System.nanoTime()-time)/1000000000.);
+
+                        // all data for the loop added to new data by this point
+                        psea.add(newData);
+                        //System.out.println((System.nanoTime()-time)/1000000);
+
+                        normed_psea.add(APAUtils.standardNormalization(newData));
+                        //System.out.println((System.nanoTime()-time)/1000000);
+
+                        center_normed_psea.add(APAUtils.centerNormalization(newData));
+                        //System.out.println((System.nanoTime()-time)/1000000);
+
+                        rank_psea.add(APAUtils.rankPercentile(newData));
+                        //System.out.println((System.nanoTime()-time)/1000000);
+
+                        enhancement.add(APAUtils.peakEnhancement(newData));
+                        //System.out.println((System.nanoTime()-time)/1000000);
+                    }
+
+
+                    gw_psea.add(psea);
+                    gw_normed_psea.add(normed_psea);
+                    gw_center_normed_psea.add(center_normed_psea);
+                    gw_rank_psea.add(rank_psea);
+                    gw_enhancement.addAll(enhancement);
+
+                    double npeaks_used_inv = 1. / npeaks_used;
+                    normed_psea.scalarMultiply(npeaks_used_inv);
+                    center_normed_psea.scalarMultiply(npeaks_used_inv);
+                    rank_psea.scalarMultiply(npeaks_used_inv);
+
+                    if (saveAllData)
+                        saveDataSet(window, psea, normed_psea, rank_psea, enhancement, chr.getName());
                 }
-
-
-                GW_psea.add(psea);
-                GW_normed_psea.add(normed_psea);
-                GW_center_normed_psea.add(center_normed_psea);
-                GW_rank_psea.add(rank_psea);
-                // TODO GW_enhancement;
-
-                double scalar = 1./npeaks_used;
-                normed_psea.scalarMultiply(scalar);
-                center_normed_psea.scalarMultiply(scalar);
-                rank_psea.scalarMultiply(scalar);
-
-                saveDataSet(window, psea, normed_psea, rank_psea, enhancement, ""+chr.getName());
-
             }
         }
         catch (IOException e){
@@ -214,34 +226,40 @@ public class APA extends JuiceboxCLT {
 
 
 
-        double GW_npeaks_used_inv = 1./GW_npeaks_used;
-        GW_normed_psea.scalarMultiply(GW_npeaks_used_inv);
-        GW_center_normed_psea.scalarMultiply(GW_npeaks_used_inv);
-        GW_rank_psea.scalarMultiply(GW_npeaks_used_inv);
+        double gw_npeaks_used_inv = 1./gw_npeaks_used;
+        gw_normed_psea.scalarMultiply(gw_npeaks_used_inv);
+        gw_center_normed_psea.scalarMultiply(gw_npeaks_used_inv);
+        gw_rank_psea.scalarMultiply(gw_npeaks_used_inv);
 
+        saveDataSet(window, gw_psea, gw_normed_psea, gw_rank_psea, gw_enhancement, "gw");
 
-        saveDataSet(window, GW_psea, GW_normed_psea, GW_rank_psea, GW_enhancement, "GW");
-
-        System.err.println("This method is not currently implemented.");
         System.exit(7);
     }
 
 
 
-
-    private Array2DRowRealMatrix cleanArray2DMatrix(int rows, int cols){
-        Array2DRowRealMatrix matrix = new Array2DRowRealMatrix(rows,cols);
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++)
-                matrix.setEntry(r,c,0);
-        return matrix;
+    //http://stackoverflow.com/questions/7574311/efficiently-compute-intersection-of-two-sets-in-java
+    private static Set<Chromosome> getSetIntersection (Set<Chromosome> set1, Set<Chromosome> set2) {
+        boolean set1IsLarger = set1.size() > set2.size();
+        Set<Chromosome> cloneSet = new HashSet<Chromosome>(set1IsLarger ? set2 : set1);
+        cloneSet.retainAll(set1IsLarger ? set1 : set2);
+        return cloneSet;
     }
 
 
     private void saveDataSet(int mdpt, Array2DRowRealMatrix psea, Array2DRowRealMatrix normed_psea,
-                             Array2DRowRealMatrix rank_psea, double[] enhancement, String prefix){
+                             Array2DRowRealMatrix rank_psea, List<Double> enhancement, String prefix){
 
-        dataFolder += "."+prefix + new SimpleDateFormat("yyyy.MM.dd.HH.mm").format(new Date());
+        File dataDirHome = new File(files[2]);
+        if(!dataDirHome.exists()){
+            boolean result = dataDirHome.mkdir();
+            if(!result){
+                System.out.println("Error creating directory (data not saved): " + dataDirHome);
+                return;
+            }
+        }
+
+        dataFolder = dataDirHome.getAbsolutePath()+"/"+prefix +"_"+ new SimpleDateFormat("yyyy.MM.dd.HH.mm").format(new Date());
         File dataDir = new File(dataFolder);
         boolean result = dataDir.mkdir();
         if(!result){
@@ -251,11 +269,11 @@ public class APA extends JuiceboxCLT {
 
         System.out.println("Saving "+prefix+" data to " + dataFolder);
 
-        //save GW data
+        //save gw data
         APAUtils.saveMatrixText(dataFolder + "/"+prefix+"_psea.txt", psea);
         APAUtils.saveMatrixText(dataFolder + "/"+prefix+"_normed_psea.txt", normed_psea);
         APAUtils.saveMatrixText(dataFolder + "/"+prefix+"_rank_psea.txt", rank_psea);
-        APAUtils.saveArrayText(dataFolder + "/" + prefix + "_enhancement.txt", enhancement);
+        APAUtils.saveListText(dataFolder + "/" + prefix + "_enhancement.txt", enhancement);
         APAUtils.saveMeasures(psea, mdpt, dataFolder + "/"+prefix+"_measures.txt");
     }
 }
