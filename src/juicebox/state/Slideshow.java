6664b345917968ae01093e6634d83e50c7185527
zgire <zulkifl.g@gmail.com>
2015-08-03 12:30:32 -0500

    Code Cleanup and Slideshow Update

--- src/juicebox/state/Slideshow.java
+++ src/juicebox/state/Slideshow.java
@@ -23,17 +23,20 @@
  */
 
 package juicebox.state;
-
-import juicebox.HiC;
 import juicebox.HiCGlobals;
 import juicebox.MainWindow;
+import juicebox.HiC;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.swing.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -48,11 +51,16 @@ public class Slideshow {
     private static MainWindow mainWindow;
     private static String statesForSlideshow = HiCGlobals.xmlSavedStatesFileName;
     private static HiC hic;
+    final static JFrame carouselFrame = new JFrame();
+    final static JPanel nextPanel = new JPanel(new BorderLayout());
+    final static JPanel prevPanel = new JPanel(new BorderLayout());
+    final static JButton nextButton = new JButton("Next State");
+    final static JButton prevButton = new JButton("Previous State");
 
 
     public static void viewShow() {
-        try {
-            ArrayList<String> savedStatePaths = new ArrayList<String>();
+         try {
+             ArrayList<String> savedStatePaths = new ArrayList<String>();
             Document dom;
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = null;
@@ -62,8 +70,27 @@ public class Slideshow {
             for (int i = 0; i < nodeList.getLength(); i++) {
                 savedStatePaths.add(nodeList.item(i).getAttributes().getNamedItem("SelectedPath").getNodeValue());
             }
-            System.out.println(savedStatePaths);
+             System.out.println(savedStatePaths);
 
+             carouselFrame.setLayout(new FlowLayout());
+             carouselFrame.setResizable(true);
+             carouselFrame.setVisible(true);
+             carouselFrame.setSize(200, 200);
+             carouselFrame.add(nextPanel);
+             carouselFrame.add(prevPanel);
+
+             nextPanel.add(nextButton,BorderLayout.EAST);
+             nextPanel.setVisible(true);
+
+             prevPanel.add(prevButton,BorderLayout.WEST);
+             prevPanel.setVisible(false);
+
+             nextButton.addActionListener(new ActionListener() {
+                 @Override
+                 public void actionPerformed(ActionEvent e) {
+                     prevPanel.setVisible(true);
+                 }
+             });
 
              /*for(String mapPath: savedStatePaths){
 
@@ -75,5 +102,7 @@ public class Slideshow {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
