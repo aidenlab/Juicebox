f885b676e9685669217886cd979f9fc4ecfd6d97
musianat <nathanielmusial@gmail.com>
2017-06-09 16:21:52 -0500

    changed apa variables back to private and added an initialize directly function

--- src/juicebox/tools/clt/juicer/APA.java
+++ src/juicebox/tools/clt/juicer/APA.java
@@ -31,6 +31,7 @@ import juicebox.data.*;
 import juicebox.tools.clt.CommandLineParserForJuicer;
 import juicebox.tools.clt.JuicerCLT;
 import juicebox.tools.utils.juicer.apa.APADataStack;
+import juicebox.tools.utils.juicer.apa.APARegionStatistics;
 import juicebox.tools.utils.juicer.apa.APAUtils;
 import juicebox.track.feature.Feature2D;
 import juicebox.track.feature.Feature2DList;
@@ -107,16 +108,16 @@ import java.util.*;
  */
 public class APA extends JuicerCLT {
     private boolean saveAllData = false;
-    private String hicFilePaths, loopListPath;
-    private File outputDirectory;
+    private String hicFilePaths, loopListPath; //TODO change to private
+    private File outputDirectory; //TODO change to private
 
     //defaults
     // TODO right now these units are based on n*res/sqrt(2)
     // TODO the sqrt(2) scaling should be removed (i.e. handle scaling internally)
-    private double minPeakDist = 30; // distance between two bins, can be changed in opts
-    private double maxPeakDist = Double.POSITIVE_INFINITY;
+    private double minPeakDist = 30; // distance between two bins, can be changed in opts //TODO change to private
+    private double maxPeakDist = Double.POSITIVE_INFINITY; //TODO change to private
     private int window = 10;
-    private int[] resolutions = new int[]{25000, 10000, 5000};
+    private int[] resolutions = new int[]{25000, 10000, 5000};//TODO change to private
     private int[] regionWidths = new int[]{6, 6, 3};
     private boolean includeInterChr = false;
 
@@ -128,6 +129,23 @@ public class APA extends JuicerCLT {
                 " [-k NONE/VC/VC_SQRT/KR] [-q corner_width] [-e include_inter_chr] [-u save_all_data]" +
                 " <hicFile(s)> <PeaksFile> <SaveFolder>");
         HiCGlobals.useCache = false;
+    }
+
+
+    public void initializeDirectly(String inputHiCFileName, String inputPeaksFile, String outputDirectoryPath, int[] resolutions,double
+            minPeakDist, double maxPeakDist){
+        this.resolutions=resolutions;
+        this.hicFilePaths=inputHiCFileName;
+        this.loopListPath=inputPeaksFile;
+        outputDirectory = HiCFileTools.createValidDirectory(outputDirectoryPath);
+
+        this.minPeakDist=minPeakDist;
+        this.maxPeakDist=maxPeakDist;
+
+        //ds = HiCFileTools.extractDatasetForCLT(Arrays.asList(inputHiCFileName.split("\\+")), true);
+        // outputDirectory = HiCFileTools.createValidDirectory(outputDirectoryPath);
+
+
     }
 
     public static String getBasicUsage() {
@@ -185,6 +203,13 @@ public class APA extends JuicerCLT {
 
     @Override
     public void run() {
+        runWithReturn();
+    }
+
+
+    public APARegionStatistics runWithReturn() {
+
+        APARegionStatistics result=null;
 
         //Calculate parameters that will need later
         int L = 2 * window + 1;
@@ -220,7 +245,7 @@ public class APA extends JuicerCLT {
 
             // Metrics resulting from apa filtering
             final Map<String, Integer[]> filterMetrics = new HashMap<>();
-
+            //looplist is empty here why??
             Feature2DList loopList = Feature2DParser.loadFeatures(loopListPath, handler, false,
                     new FeatureFilter() {
                         // Remove duplicates and filters by size
@@ -294,6 +319,8 @@ public class APA extends JuicerCLT {
                     }
                 }
                 System.out.println("Exporting APA results...");
+                //save data as int array
+                 result= APADataStack.retreiveDataStatistics(currentRegionWidth); //should retrieve data
                 APADataStack.exportGenomeWideData(gwPeakNumbers, currentRegionWidth, saveAllData);
                 APADataStack.clearAllData();
             } else {
@@ -302,5 +329,7 @@ public class APA extends JuicerCLT {
             }
         }
         System.out.println("APA complete");
+        return result;
+        //if no data return null
     }
 }
