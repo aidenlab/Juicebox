c3e4c1482b27044eec0b1f41748aa242650e82cb
nchernia <nchernia@gmail.com>
2017-07-04 11:04:51 +0200

    Added in the software used to create this .hic file as a key/attribute

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -67,7 +67,7 @@ public class HiCGlobals {
             MatrixType.VS, MatrixType.RATIO, MatrixType.OEVS, MatrixType.PEARSONVS, MatrixType.DIFF};
     public static final String defaultPropertiesURL = "http://hicfiles.tc4ga.com/juicebox.properties";
     // Juicebox version (for display purposes only)
-    public static final String versionNum = "1.7.4";
+    public static final String versionNum = "1.7.5";
     // Juicebox title
     // TODO decide on title displayed in Juicebox
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
