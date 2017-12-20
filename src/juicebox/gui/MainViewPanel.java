141151c3709be5ae6f605a5bc86a07ce38b197af
Jay Ryu <bluejay9676@gmail.com>
2017-12-20 17:46:50 +0900

    Version 4 - 2
    - Import error fixed.

--- src/juicebox/gui/MainViewPanel.java
+++ src/juicebox/gui/MainViewPanel.java
@@ -51,6 +51,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
