720fa357dd28039b00434db2aeb1bc0f484cd2f3
Muhammad Saad Shamim <sa501428@gmail.com>
2019-03-20 02:41:02 -0500

    update version number

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -38,7 +38,7 @@ import java.util.List;
 public class HiCGlobals {
 
     // Juicebox version (for display and header purposes only)
-    public static final String versionNum = "1.9.14"; //
+    public static final String versionNum = "1.10.01"; //
 
     // Changes Data Output Mode
     public static final boolean isRestricted = false;
