436a15b70c920ae87276687211511fcfeebc5073
tiendatngcs <44583359+tiendatngcs@users.noreply.github.com>
2019-07-16 16:00:20 -0500

    Modified Domain Finder

--- src/juicebox/tools/utils/juicer/grind/DomainFinder.java
+++ src/juicebox/tools/utils/juicer/grind/DomainFinder.java
@@ -24,24 +24,138 @@
 
 package juicebox.tools.utils.juicer.grind;
 
-import juicebox.data.Dataset;
+import juicebox.data.*;
+import juicebox.tools.utils.common.MatrixTools;
+import juicebox.track.feature.Feature2D;
 import juicebox.track.feature.Feature2DList;
+import juicebox.track.feature.FeatureFunction;
+import juicebox.windowui.HiCZoom;
 import juicebox.windowui.NormalizationType;
+import org.apache.commons.math.linear.RealMatrix;
+import org.broad.igv.feature.Chromosome;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
-public class DomainFinder implements RegionFinder {
+import static juicebox.tools.utils.juicer.grind.SectionParser.saveMatrixText2;
 
+public class DomainFinder implements RegionFinder {
+    Exception ex
+    private int x;
+    private int y;
+    private int z;
+    private Dataset ds;
+    private Feature2DList features;
+    private String path;
+    private File outputDirectory;
+    private Set<String> givenChromosomes;
+    private NormalizationType norm;
+    private boolean useObservedOverExpected;
+    private boolean useDenseLabels;
+    private Set<Integer> resolutions;
+    private int x_dim;
+    private int y_dim;
+
+    {
+        ex.printStackTrace();
+    } catch(
 
     public DomainFinder(int x, int y, int z, Dataset ds, Feature2DList features, File outputDirectory, Set<String> givenChromosomes, NormalizationType norm,
                         boolean useObservedOverExpected, boolean useDenseLabels, Set<Integer> resolutions) {
-    }
-
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.ds = ds;
+        this.features = features;
+        this.path = outputDirectory.getPath();
+        this.path = outputDirectory.getPath();
+        this.outputDirectory = outputDirectory;
+        this.givenChromosomes = givenChromosomes;
+        this.norm = norm;
+        this.useObservedOverExpected = useObservedOverExpected;
+        this.useDenseLabels = useDenseLabels;
+        this.resolutions = resolutions;
+        //this.x_dim = ds;
+    })
 
     @Override
-    public void makePositiveExamples(String savepath, String loopListPath, int maxk, String hicFilePaths) {
+    public void makePositiveExamples(String savepath, String loopListPath, String hicFilePaths) {
+        final Random generator = new Random();
 
+        //String loopListPath = "";
+
+        File file = new File(path);
+        if (!file.isDirectory()) {
+            file.mkdir();
+        }
+
+
+        final ChromosomeHandler chromosomeHandler = ds.getChromosomeHandler();
+
+        final int resolution = (int) resolutions.toArray()[0];
+
+        final int halfWidthI = x / 2;
+        final int halfWidthJ = y / 2;
+        final int maxk = z / features.getNumTotalFeatures();
+
+        try {
+            final Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path + "all_file_names.txt"), StandardCharsets.UTF_8));
+
+            // Feature2DList features = Feature2DParser.loadFeatures(loopListPath, chromosomeHandler, false, null, false);
+
+            features.processLists(new FeatureFunction() {
+                @Override
+                public void process(String chr, List<Feature2D> feature2DList) {
+
+                    System.out.println("Currently on: " + chr);
+
+                    Chromosome chrom = chromosomeHandler.getChromosomeFromName((String) givenChromosomes.g);
+
+                    Matrix matrix = ds.getMatrix(chrom, chrom);
+                    if (matrix == null) return;
+
+                    HiCZoom zoom = ds.getZoomForBPResolution(resolution);
+                    final MatrixZoomData zd = matrix.getZoomData(zoom);
+
+                    if (zd == null) return;
+
+
+                    for (Feature2D feature2D : feature2DList) {
+                        int i0 = feature2D.getStart1() / resolution;
+                        int j0 = feature2D.getEnd1() / resolution;
+
+
+                        try {
+                            RealMatrix localizedRegionData = HiCFileTools.extractLocalBoundedRegion(zd,
+                                    i, i + x,
+                                    j, j + y, x, y, norm);
+                            if (MatrixTools.sum(localizedRegionData.getData()) > 0) {
+
+                                String exactFileName = chrom.getName() + "_" + i + "_" + j + ".txt";
+
+                                //process
+                                //DescriptiveStatistics yStats = statistics(localizedRegionData.getData());
+                                //mm = (m-yStats.getMean())/Math.max(yStats.getStandardDeviation(),1e-7);
+                                //ZscoreLL = (centralVal - yStats.getMean()) / yStats.getStandardDeviation();
+
+                                saveMatrixText2(path + exactFileName, localizedRegionData);
+                                writer.write(exactFileName + "\n");
+                            }
+                        } catch (Exception e) {
+
+                        }
+                    }
+                }
+
+            }
+        })
+
+        writer.close();
+    }
+}
     }
 
     @Override
