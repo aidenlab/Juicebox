9c65144e9923940fb5ce647eec89f962be63027a
Muhammad Saad Shamim <sa501428@gmail.com>
2020-02-10 23:54:56 -0600

    read in and collapse bed file

--- src/juicebox/tools/utils/dev/drink/DrinkUtils.java
+++ src/juicebox/tools/utils/dev/drink/DrinkUtils.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2011-2019 Broad Institute, Aiden Lab
+ * Copyright (c) 2011-2020 Broad Institute, Aiden Lab
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,9 +24,19 @@
 
 package juicebox.tools.utils.dev.drink;
 
+import juicebox.HiCGlobals;
+import juicebox.data.ChromosomeHandler;
+import juicebox.data.HiCFileTools;
 import juicebox.data.feature.FeatureFilter;
 import juicebox.data.feature.GenomeWideList;
+import org.broad.igv.Globals;
+import org.broad.igv.feature.Chromosome;
+import org.broad.igv.util.ParsingUtils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class DrinkUtils {
@@ -125,5 +135,95 @@ public class DrinkUtils {
         mostFrequentSubcompartment.addAll(frequentFliers);
         */
 
+    public static void readInFileAndCollapse(String location) {
+
+        ChromosomeHandler handler = HiCFileTools.loadChromosomes("hg19");
+        GenomeWideList<SubcompartmentInterval> subc = loadFromSubcompartmentBEDFile(handler, location);
+        DrinkUtils.collapseGWList(subc);
+        subc.simpleExport(new File(location + "_collapsed.bed"));
+    }
+
+
+    /**
+     * @param handler
+     * @param bedFilePath
+     * @return List of motif anchors from the provided bed file
+     */
+    public static GenomeWideList<SubcompartmentInterval> loadFromSubcompartmentBEDFile(ChromosomeHandler handler, String bedFilePath) {
+        List<SubcompartmentInterval> anchors = new ArrayList<>();
+
+        try {
+            //BufferedReader br = ParsingUtils.openBufferedReader(bedFilePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(ParsingUtils.openInputStream(bedFilePath)), HiCGlobals.bufferSize);
+            anchors.addAll(parseSubcompartmentBEDFile(br, handler));
+        } catch (IOException ec) {
+            ec.printStackTrace();
+        }
+
+        return new GenomeWideList<>(handler, anchors);
+    }
+
+    /**
+     * Methods for handling BED Files
+     */
+
+    /**
+     * Helper function for actually parsing BED file
+     * Ignores any attributes beyond the third column (i.e. just chr and positions are read)
+     *
+     * @param bufferedReader
+     * @param handler
+     * @return list of motifs
+     * @throws IOException
+     */
+    private static List<SubcompartmentInterval> parseSubcompartmentBEDFile(BufferedReader bufferedReader, ChromosomeHandler handler) throws IOException {
+        Set<SubcompartmentInterval> anchors = new HashSet<>();
+        String nextLine;
+
+        Map<String, Integer> allIdsToIntId = new HashMap<>();
+        int counter = 1;
+
+        int errorCount = 0;
+        while ((nextLine = bufferedReader.readLine()) != null) {
+            String[] tokens = Globals.tabPattern.split(nextLine);
+
+            if (tokens.length > 3 && tokens[3].equalsIgnoreCase("NA")) {
+                continue;
+            }
+
+            if (tokens[0].startsWith("chr") && tokens.length > 2) {
+                // valid line
+                String chr1Name = tokens[0];
+                int start1 = Integer.parseInt(tokens[1]);
+                int end1 = Integer.parseInt(tokens[2]);
+                String id = tokens[3].toUpperCase();
+
+                if (!allIdsToIntId.containsKey(id)) {
+                    allIdsToIntId.put(id, counter);
+                    System.out.println(id + "  " + counter);
+                    counter++;
+                }
+                int val = allIdsToIntId.get(id);
+
+
+                Chromosome chr = handler.getChromosomeFromName(chr1Name);
+                if (chr == null) {
+                    if (errorCount < 10) {
+                        System.out.println("Skipping line: " + nextLine);
+                    } else if (errorCount == 10) {
+                        System.err.println("Maximum error count exceeded.  Further errors will not be logged");
+                    }
+
+                    errorCount++;
+                    continue;
+                }
+
+                anchors.add(new SubcompartmentInterval(chr.getIndex(), chr.getName(), start1, end1, val));
+            }
+        }
+        if (anchors.size() < 1) System.err.println("BED File empty - file may have problems or error was encountered");
+        bufferedReader.close();
+        return new ArrayList<>(anchors);
+    }
 
 }
