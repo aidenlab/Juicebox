654d60f818fb91e818f3b8a2dfda1d1fa23d42bd
Muhammad Saad Shamim <sa501428@gmail.com>
2020-02-14 08:23:31 -0600

    apa bug fix

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -38,7 +38,7 @@ import java.util.List;
 public class HiCGlobals {
 
     // Juicebox version (for display and header purposes only)
-    public static final String versionNum = "1.18.08";
+    public static final String versionNum = "1.18.10";
     // Juicebox title
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
