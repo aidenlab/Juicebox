513bb2211fb72926dd76e74f5e76d090750b9b66
Muhammad S Shamim <sa501428@gmail.com>
2021-02-04 14:59:03 -0600

    deprecate default building of KR

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -37,7 +37,7 @@ import java.util.concurrent.Executors;
 public class HiCGlobals {
 
     // Juicebox version (for display and header purposes only)
-    public static final String versionNum = "2.03.03";
+    public static final String versionNum = "2.03.04";
     // Juicebox title
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
