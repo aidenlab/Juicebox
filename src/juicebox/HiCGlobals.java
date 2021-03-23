414696faa3304a9894e86318becce79a0972799d
Muhammad S Shamim <sa501428@gmail.com>
2021-03-23 17:39:02 -0500

    deprecate addgwnorm (can still add gw norm via addnorm)

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -37,7 +37,7 @@ import java.util.concurrent.Executors;
 public class HiCGlobals {
 
     // Juicebox version (for display and header purposes only)
-    public static final String versionNum = "2.04.18";
+    public static final String versionNum = "2.04.19";
     // Juicebox title
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
