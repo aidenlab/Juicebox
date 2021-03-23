92a0fa87efe60fa40caa8b8d70dd1b937537e5d1
Muhammad S Shamim <sa501428@gmail.com>
2021-03-23 18:34:53 -0500

    creation of in memory storage

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -37,7 +37,7 @@ import java.util.concurrent.Executors;
 public class HiCGlobals {
 
     // Juicebox version (for display and header purposes only)
-    public static final String versionNum = "2.04.19";
+    public static final String versionNum = "2.04.20";
     // Juicebox title
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
--- src/juicebox/data/iterator/ListOfListGenerator.java
+++ src/juicebox/data/iterator/ListOfListGenerator.java
@@ -28,16 +28,80 @@ import juicebox.data.*;
 import juicebox.windowui.HiCZoom;
 import org.broad.igv.util.collections.LRUCache;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ListOfListGenerator {
     public static IteratorContainer createFromZD(DatasetReader reader, MatrixZoomData matrixZoomData,
                                                  LRUCache<String, Block> blockCache, boolean saveAllIntoRAM) {
         IteratorContainer ic = new ZDIteratorContainer(reader, matrixZoomData, blockCache);
-        return ic;
+        return tryToCreateIteratorInRAM(ic, saveAllIntoRAM);
     }
 
     public static IteratorContainer createForWholeGenome(Dataset dataset, ChromosomeHandler chromosomeHandler,
                                                          HiCZoom zoom, boolean includeIntraData, boolean saveAllIntoRAM) {
         IteratorContainer ic = new GWIteratorContainer(dataset, chromosomeHandler, zoom, includeIntraData);
-        return ic;
+        return tryToCreateIteratorInRAM(ic, saveAllIntoRAM);
+    }
+
+    private static IteratorContainer tryToCreateIteratorInRAM(IteratorContainer ic0, boolean saveAllIntoRAM) {
+        if (saveAllIntoRAM) {
+            try {
+                // we should count once to ensure this is reasonable to do so memory-wise
+                boolean shouldFitInMemory = checkMemory(ic0);
+                if (shouldFitInMemory) {
+                    List<List<ContactRecord>> allContactRecords = populateListOfLists(ic0);
+                    long numOfContactRecords = getTotalSize(allContactRecords);
+
+                    IteratorContainer newIC = new ListOfListIteratorContainer(allContactRecords,
+                            ic0.getMatrixSize(),
+                            numOfContactRecords);
+                    return newIC;
+                }
+            } catch (Exception e) {
+                System.err.println(e.getLocalizedMessage());
+                System.err.println("Will use default iterator");
+            }
+        }
+
+        return ic0;
+    }
+
+    private static List<List<ContactRecord>> populateListOfLists(IteratorContainer ic) {
+        List<List<ContactRecord>> allRecords = new ArrayList<>();
+        List<ContactRecord> tempList = new ArrayList<>();
+        int counter = 0;
+
+        Iterator<ContactRecord> iterator = ic.getNewContactRecordIterator();
+        while (iterator.hasNext()) {
+            tempList.add(iterator.next());
+            counter++;
+            if (counter > Integer.MAX_VALUE - 10) {
+                allRecords.add(tempList);
+                tempList = new ArrayList<>();
+                counter = 0;
+            }
+        }
+
+        if (tempList.size() > 0) {
+            allRecords.add(tempList);
+        }
+
+        return allRecords;
+    }
+
+    private static boolean checkMemory(IteratorContainer ic) {
+        long ramForRowSums = ic.getMatrixSize() * 4;
+        long ramForAllContactRecords = ic.getNumberOfContactRecords() * 12;
+        return ramForRowSums + ramForAllContactRecords < Runtime.getRuntime().maxMemory();
+    }
+
+    private static long getTotalSize(List<List<ContactRecord>> allContactRecords) {
+        long numOfContactRecords = 0;
+        for (List<ContactRecord> records : allContactRecords) {
+            numOfContactRecords += records.size();
+        }
+        return numOfContactRecords;
     }
 }
