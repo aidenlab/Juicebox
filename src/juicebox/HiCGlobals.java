b43e488d8cfbd3b0486070656d13a23c69512030
Muhammad S Shamim <sa501428@gmail.com>
2021-07-13 23:09:19 -0500

    bug fix in pre

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -36,7 +36,7 @@ import java.util.concurrent.Executors;
  */
 public class HiCGlobals {
 
-    public static final String versionNum = "2.09.00";
+    public static final String versionNum = "2.10.01";
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
     // MainWindow variables
