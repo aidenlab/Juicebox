2b0e323c27bdee1bd483215b3ddb3b92d96bbf7e
Muhammad S Shamim <sa501428@gmail.com>
2021-09-17 00:26:06 -0500

    revert calculation

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -36,7 +36,7 @@ import java.util.concurrent.Executors;
  */
 public class HiCGlobals {
 
-    public static final String versionNum = "2.13.05";
+    public static final String versionNum = "2.13.06";
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
     // MainWindow variables
