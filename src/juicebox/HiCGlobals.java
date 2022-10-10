cd25409cf09d5f1bae458a35bffa8959ac34519d
Muhammad Saad Shamim <sa501428@gmail.com>
2022-10-09 21:56:55 -0500

    updated version of normalization from the hictools folder

--- src/juicebox/HiCGlobals.java
+++ src/juicebox/HiCGlobals.java
@@ -36,7 +36,7 @@ import java.util.concurrent.Executors;
  */
 public class HiCGlobals {
 
-    public static final String versionNum = "2.18.00";
+    public static final String versionNum = "2.20.00";
     public static final String juiceboxTitle = "[Juicebox " + versionNum + "] Hi-C Map ";
 
     // MainWindow variables
