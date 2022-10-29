da75f82e5dae83e39efa11f8cf74f0cdeb5a8262
Muhammad Saad Shamim <sa501428@gmail.com>
2022-10-29 11:31:43 -0500

    some fixes

--- src/juicebox/AnnotatedExample.java
+++ src/juicebox/AnnotatedExample.java
@@ -1,9 +1,32 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011-2022 Broad Institute, Aiden Lab, Rice University, Baylor College of Medicine
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
 package juicebox;
 
 import javastraw.reader.Dataset;
 import javastraw.reader.basics.Chromosome;
 import javastraw.reader.block.Block;
-import javastraw.reader.block.ContactRecord;
 import javastraw.reader.mzd.Matrix;
 import javastraw.reader.mzd.MatrixZoomData;
 import javastraw.reader.norm.NormalizationPicker;
@@ -11,7 +34,6 @@ import javastraw.reader.type.HiCZoom;
 import javastraw.reader.type.NormalizationType;
 import javastraw.tools.HiCFileTools;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class AnnotatedExample {
@@ -21,54 +43,43 @@ public class AnnotatedExample {
         // query nearby regions, it can improve speed by a lot
         boolean useCache = false;
         String filename = "https://www.dropbox.com/s/a6ykz8ajgszv0b6/Trachops_cirrhosus.rawchrom.hic";
+        filename = juicebox.data.HiCFileTools.cleanUpDropboxURL(filename);
 
         // create a hic dataset object
 
         long s1 = System.nanoTime();
         Dataset ds = HiCFileTools.extractDatasetForCLT(filename, false, useCache, false);
         long s2 = System.nanoTime();
-        System.out.println((s2-s1)*1e-9);
-        System.out.println("^^^^ line 28 execution line");
+        System.out.println((s2 - s1) * 1e-9);
+        System.out.println("^^^^ line 29 execution line");
 
         // pick the normalization we would like
         // this line will check multiple possible norms
         // and pick whichever is available (in order of preference)
 
-        long s3 = System.nanoTime();
         NormalizationType norm = NormalizationPicker.getFirstValidNormInThisOrder(ds, new String[]{"KR", "SCALE", "VC", "VC_SQRT", "NONE"});
-        long s4 = System.nanoTime();
-        System.out.println((s4-s3)*1e-9);
-        System.out.println("^^^^ line 40 execution line");
         System.out.println("Norm being used: " + norm.getLabel());
 
-        // let's set our resolution
         int resolution = 5000;
-
-        // let's grab the chromosomes
-
-        long s5 = System.nanoTime();
         Chromosome[] chromosomes = ds.getChromosomeHandler().getChromosomeArrayWithoutAllByAll();
-        long s6 = System.nanoTime();
-        System.out.println((s6-s5)*1e-9);
-        System.out.println("^^^^ line 52 execution line");
 
 
         // now let's iterate on every chromosome (only intra-chromosomal regions for now)
         for (Chromosome chromosome : chromosomes) {
+            if (chromosome.getIndex() > 6) continue;
             long s7 = System.nanoTime();
             Matrix matrix = ds.getMatrix(chromosome, chromosome);
             long s8 = System.nanoTime();
-            System.out.println((s8-s7)*1e-9);
-            System.out.println("^^^^ line 59 execution line");
-
+            System.out.println((s8 - s7) * 1e-9);
+            System.out.println("^^^^ line 49 execution line");
 
             if (matrix == null) continue;
             long s9 = System.nanoTime();
 
             MatrixZoomData zd = matrix.getZoomData(new HiCZoom(resolution));
             long s10 = System.nanoTime();
-            System.out.println((s10-s9)*1e-9);
-            System.out.println("^^^^ line 70 execution line");
+            System.out.println((s10 - s9) * 1e-9);
+            System.out.println("^^^^ line 57 execution line");
 
             if (zd == null) continue;
 
@@ -87,31 +98,19 @@ public class AnnotatedExample {
             long s11 = System.nanoTime();
             List<Block> blocks = zd.getNormalizedBlocksOverlapping(binXStart, binYStart, binXEnd, binYEnd, norm, getDataUnderTheDiagonal);
             long s12 = System.nanoTime();
-            System.out.println((s12-s11)*1e-9);
+            System.out.println((s12 - s11) * 1e-9);
+            System.out.println("^^^^ line 77 execution line");
+
+
+            binXStart = 1500;
+            binYStart = 1600;
+            binXEnd = 2000;
+            binYEnd = 2200;
+            s11 = System.nanoTime();
+            blocks = zd.getNormalizedBlocksOverlapping(binXStart, binYStart, binXEnd, binYEnd, norm, getDataUnderTheDiagonal);
+            s12 = System.nanoTime();
+            System.out.println((s12 - s11) * 1e-9);
             System.out.println("^^^^ line 88 execution line");
-
-
-            long s13 = System.nanoTime();
-            for (Block b : blocks) {
-                if (b != null) {
-                    for (ContactRecord rec : b.getContactRecords()) {
-                        if (rec.getCounts() > 0) { // will skip NaNs
-                            // can choose to use the BIN coordinates
-                            int binX = rec.getBinX();
-                            int binY = rec.getBinY();
-
-                            // you could choose to use relative coordinates for the box given
-                            int relativeX = rec.getBinX() - binXStart;
-                            int relativeY = rec.getBinY() - binYStart;
-
-                            float counts = rec.getCounts();
-                        }
-                    }
-                }
-            }
-            long s14 = System.nanoTime();
-            System.out.println((s14-s13)*1e-9);
-            System.out.println("^^^^ for loop execution line");
         }
     }
 }
