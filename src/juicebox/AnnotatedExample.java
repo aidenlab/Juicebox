7c530c79994b4d94bf3730eb60a096ae2fd4fbdb
MahdiSayadSadr <61241675+MahdiSayadSadr@users.noreply.github.com>
2022-11-01 09:08:55 -0500

    updating importa

--- src/juicebox/AnnotatedExample.java
+++ src/juicebox/AnnotatedExample.java
@@ -34,19 +34,9 @@ import juicebox.windowui.NormalizationHandler;
 import juicebox.windowui.NormalizationType;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-
-import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class AnnotatedExample {
     public static void main(String[] args) throws IOException {
